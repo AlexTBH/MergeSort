0dbfd694e6a74a4acfe5fd5bf5e0053bff632771
AlexTBHoang <alexanderhoangtb@gmail.com>
2022-12-19 17:12:56 +0100

    commit

--- mergesort.rb
+++ mergesort.rb
@@ -1,29 +1,14 @@
-def mergeSort(input)
-    #Basecase
-    #When there is only one element left in the array
-    #Keep recursing until input.length < 1, return
+def mergeSort(arr)
 
-    #Alternitave 1 
-    #need to check if input is even or odd
-        #var1
-        #if even?
-            
-                #compare input
-                #var1 = the smaller array
-                #mergesort (var1)
-        #if odd?
-            #WHAT TO DO WITH AN ODD ARRAY?
-            #Need to split the array to one even array and one odd array
-                
-            
-        #compare two arrays, sort the array
-        #mergeSort(var1)
-    #Alternitave 1
+    unless arr.length < 1
+        
+    end
 
-    #----------------------------------------------------------
-
-    #Alternitave 2
+    puts arr.each_slice((arr.size/2.0).round).to_a
+  
     #send both halves down the method mergeSort() and save it in a variable, for example
+    #var = arr.length/2
+
         #var1 = mergeSort(arr1)
         #var2 = mergeSort(arr2)
 
@@ -32,20 +17,26 @@ def mergeSort(input)
     #compare two arrays, sort the array, iterate throguh both arrays to compare them?
     # var1, var2
     
-
     # var1.each_with_index do |elem, i|
-    #   if var2 > var1
+    #   if var2[i] > var1[i]
     #       var1.insert(i, var2[i])
     #   end
     #end
 
-    #Alternitave 2
+    #i = 0
+    #   while input.length > n
+    #       if var2[i] > var1[i]
+    #           var1.insert(i, var2[i])
+    #       end
+    #   i++
+    #   return var1
 
+    # 1, 1, 1, 1, 9,
 
-
-
-    # 1, 1, 1, 1, 9, 
     # 2, 3, 7, 9, 10,
     
-    # 1, 1, 2, 3, 5, 6, 7, 9, 9, 10, 
+
+    
 end
+
+mergeSort([1, 2, 3, 4, 5, 6, 7, 8, 9])
